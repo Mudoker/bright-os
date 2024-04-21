ca4ea8863d2674cc45c4ee4b87bb760930543f63
Mudoker <huuquoc7603@gmail.com>
2024-04-21 21:12:42 +0700

    init: Project Set up

--- src/helper/boxer.c
+++ src/helper/boxer.c
@@ -1,12 +1,24 @@
-void print_in_box(const char *text)
-{
-    printf("┌");
-    for (int i = 0; i < strlen(text) + 2; i++)
-        printf("─");
-    printf("┐\n");
-    printf("│ %s │\n", text);
-    printf("└");
-    for (int i = 0; i < strlen(text) + 2; i++)
-        printf("─");
-    printf("┘\n");
-}
+// // Function to calculate the length of a string
+// int string_length(const char *str)
+// {
+//     int length = 0;
+//     while (*str)
+//     {
+//         length++;
+//         str++;
+//     }
+//     return length;
+// }
+
+// void print_in_box(const char *text)
+// {
+//     printf("┌");
+//     for (int i = 0; text[i] != '\0'; i++)
+//         printf("─");
+//     printf("┐\n");
+//     printf("│ %s │\n", text);
+//     printf("└");
+//     for (int i = 0; text[i] != '\0'; i++)
+//         printf("─");
+//     printf("┘\n");
+// }
--- src/helper/formater.c
+++ src/helper/formater.c
@@ -1,55 +1,74 @@
-#include "../global/headers/format.h"
+// #include "../global/headers/format.h"
 
-// Function to format string with color and print it
-void format_and_print(char *str, const char *color, int bold, int italic)
-{
-    char *color_code = "";
-    char *style_code = "";
+// int strings_are_equal(const char *str1, const char *str2)
+// {
+//     while (*str1 && *str2)
+//     {
+//         if (*str1 != *str2)
+//         {
+//             return 0; // Not equal
+//         }
+//         str1++;
+//         str2++;
+//     }
+//     // Check if both strings have ended simultaneously
+//     if (*str1 == '\0' && *str2 == '\0')
+//     {
+//         return 1; // Equal
+//     }
+//     return 0; // Not equal
+// }
 
-    // Assign color code based on the provided color string
-    if (strcmp(color, "red") == 0)
-    {
-        color_code = "\x1b[31m"; // Red
-    }
-    else if (strcmp(color, "green") == 0)
-    {
-        color_code = "\x1b[32m"; // Green
-    }
-    else if (strcmp(color, "yellow") == 0)
-    {
-        color_code = "\x1b[33m"; // Yellow
-    }
-    else if (strcmp(color, "blue") == 0)
-    {
-        color_code = "\x1b[34m"; // Blue
-    }
-    else if (strcmp(color, "magenta") == 0)
-    {
-        color_code = "\x1b[35m"; // Magenta
-    }
-    else if (strcmp(color, "cyan") == 0)
-    {
-        color_code = "\x1b[36m"; // Cyan
-    }
-    else
-    {
-        color_code = ANSI_COLOR_RESET; // Default color
-    }
+// // Function to format string with color and print it
+// void format_and_print(char *str, const char *color, int bold, int italic)
+// {
+//     char *color_code = "";
+//     char *style_code = "";
 
-    // Assign style code based on bold and italic flags
-    if (bold && italic)
-    {
-        style_code = "\x1b[1;3m"; // Bold and Italic
-    }
-    else if (bold)
-    {
-        style_code = "\x1b[1m"; // Bold
-    }
-    else if (italic)
-    {
-        style_code = "\x1b[3m"; // Italic
-    }
+//     // Assign color code based on the provided color string
+//     if (strings_are_equal(color, "red"))
+//     {
+//         color_code = "\x1b[31m"; // Red
+//     }
+//     else if (strings_are_equal(color, "green"))
+//     {
+//         color_code = "\x1b[32m"; // Green
+//     }
+//     else if (strings_are_equal(color, "yellow"))
+//     {
+//         color_code = "\x1b[33m"; // Yellow
+//     }
+//     else if (strings_are_equal(color, "blue"))
+//     {
+//         color_code = "\x1b[34m"; // Blue
+//     }
+//     else if (strings_are_equal(color, "magenta"))
+//     {
+//         color_code = "\x1b[35m"; // Magenta
+//     }
+//     else if (strings_are_equal(color, "cyan"))
+//     {
+//         color_code = "\x1b[36m"; // Cyan
+//     }
+//     else
+//     {
+//         color_code = ANSI_COLOR_RESET; // Default color
+//     }
 
-    // Print the formatted string
-    printf("%s%s%s%s%s", style_code, color_code, str, ANSI_COLOR_RESET, style_code);
-}
+//     // Assign style code based on bold and italic flags
+//     if (bold && italic)
+//     {
+//         style_code = "\x1b[1;3m"; // Bold and Italic
+//     }
+//     else if (bold)
+//     {
+//         style_code = "\x1b[1m"; // Bold
+//     }
+//     else if (italic)
+//     {
+//         style_code = "\x1b[3m"; // Italic
+//     }
+
+//     // Print the formatted string
+//     printf("%s%s%s%s%s", style_code, color_code, str, ANSI_COLOR_RESET, style_code);
+// }

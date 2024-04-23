14347fc62f4653886b9af9f6a4dfef9fb0cceb95
Mudoker <huuquoc7603@gmail.com>
2024-04-23 15:44:57 +0700

    feat: cls to clear terminal

--- src/cli/kernel.c
+++ src/cli/kernel.c
@@ -1,76 +1,81 @@
-#include "../uart/uart.h"
-#include "../global/global.h"
 #include "../commands/os.h"
+#include "../global/global.h"
+#include "../helper/styler/styler.h"
 #include "../helper/utils/utils.h"
+#include "../uart/uart.h"
 
-#define MAX_CMD_SIZE 100
+void cli() {
+  static char cli_buffer[MAX_CMD_SIZE] = {0};
+  static int index = 0;
+  static int is_new_command = 1;
 
-void cli()
-{
-	static char cli_buffer[MAX_CMD_SIZE];
-	static int index = 0;
-	static int is_new_command = 1;
+  // Show prompt only if new command
+  char *prompt = str_format("BrightOS> ", COLOR.YELLOW, 0);
+  if (is_new_command) {
+    uart_puts("\n");
+    uart_puts(prompt);
+    is_new_command = 0;
+  }
 
-	if (is_new_command == 1)
-	{
-		uart_puts("\nBrightOS> ");
-		is_new_command = 0;
-	}
+  // Get command from user
+  char c = uart_getc();
 
-	char c = uart_getc();
+  // Check for backspace
+  if (c == '\b') {
+    // Prevent backspace from deleting the prompt
+    if (index > 0) {
+      // Update the buffer and index
+      index--;
+      cli_buffer[index] = '\0';
+      uart_puts("\b \b");
+    }
+  } else {
+    // Get the command until newline
+    if (c != '\n') {
+      // Append the character to the buffer
+      if (index < MAX_CMD_SIZE - 1) {
+        cli_buffer[index] = c;
+        index++;
+        uart_sendc(c);
+      } else {
+        uart_puts("\nCommand too long. Please try again.\n");
+        index = 0;
+        is_new_command = 1;
+      }
+    } else {
+      // Add null terminator to the buffer
+      cli_buffer[index] = '\0';
 
-	// Check for backspace
-	if (c == '\b')
-	{
-		if (index > 0)
-		{
-			index--;
-			cli_buffer[index] = '\0';
-			uart_puts("\b \b");
-		}
-	}
-	else
-	{
-		if (c != '\n')
-		{
-			if (index < MAX_CMD_SIZE - 1)
-			{
-				cli_buffer[index] = c;
-				index++;
-				uart_sendc(c);
-			}
-		}
-		else
-		{
-			cli_buffer[index] = '\0';
+      // Compare and execute the command
+      if (is_equal(cli_buffer, "os") == 0) {
+        os_greet();
+      } else if (is_equal(cli_buffer, "clr") == 0 ||
+                 is_equal(cli_buffer, "cls") == 0 ||
+                 is_equal(cli_buffer, "clear") == 0) {
+        uart_puts("\033c"); // Full terminal reset
+      } else {
+        uart_puts("\nInvalid command. Type 'help' to see available commands.");
+      }
 
-			uart_puts("\nGot commands: ");
-			uart_puts(cli_buffer);
-
-			if (strcmp(cli_buffer, "os") == 0)
-			{
-				os_greet();
-			}
-
-			uart_puts("\n");
-
-			index = 0;
-			is_new_command = 1;
-		}
-	}
+      // Reset the buffer
+      uart_puts("\n");
+      index = 0;
+      is_new_command = 1;
+    }
+  }
 }
 
-int main()
-{
-	// set up serial console
-	uart_init();
+int main() {
+  // set up serial console
+  uart_init();
 
-	// Welcome message
-	os_greet();
+  // Welcome message
+  os_greet();
 
-	while (1)
-	{
-		cli();
-	}
-	return 0;
+  // OS loop
+  while (1) {
+    cli();
+  }
+
+  return 0;
 }

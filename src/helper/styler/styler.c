842a705db24952dcb67662530a2cff7bac651151
Mudoker <huuquoc7603@gmail.com>
2024-04-26 01:20:31 +0700

    feat: convert to mac from hex

--- src/helper/styler/styler.c
+++ src/helper/styler/styler.c
@@ -26,21 +26,24 @@ void str_format(char *str, const char *color_code, const char *bg_color_code) {
 void print_in_box(char *str) {
   // Print the formatted string in a box
   uart_puts("\n");
-  uart_puts("+");
+  str_format("+", OS_CONFIG.SECONDARY_COLOR, OS_CONFIG.BACKGROUND_COLOR);
   for (int i = 0; i < len(str) + 2; i++) {
-    uart_puts("-");
+    str_format("-", OS_CONFIG.SECONDARY_COLOR, OS_CONFIG.BACKGROUND_COLOR);
   }
-  uart_puts("+\n");
+  str_format("+", OS_CONFIG.SECONDARY_COLOR, OS_CONFIG.BACKGROUND_COLOR);
+  uart_puts("\n");
 
-  uart_puts("| ");
-  uart_puts(str);
-  uart_puts(" |\n");
+  str_format("| ", OS_CONFIG.SECONDARY_COLOR, OS_CONFIG.BACKGROUND_COLOR);
+  str_format(str, OS_CONFIG.PRIMARY_COLOR, OS_CONFIG.BACKGROUND_COLOR);
+  str_format(" |", OS_CONFIG.SECONDARY_COLOR, OS_CONFIG.BACKGROUND_COLOR);
+  uart_puts("\n");
 
-  uart_puts("+");
+  str_format("+", OS_CONFIG.SECONDARY_COLOR, OS_CONFIG.BACKGROUND_COLOR);
   for (int i = 0; i < len(str) + 2; i++) {
-    uart_puts("-");
+    str_format("-", OS_CONFIG.SECONDARY_COLOR, OS_CONFIG.BACKGROUND_COLOR);
   }
-  uart_puts("+\n");
+  str_format("+", OS_CONFIG.SECONDARY_COLOR, OS_CONFIG.BACKGROUND_COLOR);
+  uart_puts("\n");
 }
 
 void tabulate(char *keys[], int numKeys, char *values[][MAX_ROWS],
@@ -58,20 +61,22 @@ void tabulate(char *keys[], int numKeys, char *values[][MAX_ROWS],
 
   // Print keys
   for (int i = 0; i < numKeys; i++) {
-    // str_format(keys[i], OS_CONFIG.PRIMARY_COLOR);
+    str_format(keys[i], OS_CONFIG.PRIMARY_COLOR, OS_CONFIG.BACKGROUND_COLOR);
     // Adjust spacing based on the maximum length of the column
     for (int j = 0; j < maxColLength[i] - len(keys[i]) + 4;
          j++) { // Added 4 spaces for padding
       uart_puts(" ");
     }
   }
+
   uart_puts("\n\n");
 
   // Print values
   for (int i = 0; i < numValues; i++) {
     // Print values for current row
     for (int j = 0; j < numKeys && values[i][j] != (char *)0; j++) {
-      // str_format(values[i][j], OS_CONFIG.SECONDARY_COLOR);
+      str_format(values[i][j], OS_CONFIG.SECONDARY_COLOR,
+                 OS_CONFIG.BACKGROUND_COLOR);
       // Adjust spacing based on the maximum length of the column
       for (int k = 0; k < maxColLength[j] - len(values[i][j]) + 4;
            k++) { // Added 4 spaces for padding
